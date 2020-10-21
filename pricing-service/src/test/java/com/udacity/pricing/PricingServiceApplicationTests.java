70013b3438cec64089d0b6f379ca69b5d2c93d12
bolf <bolf@li.ru>
2020-10-21 23:40:20 +0300

    pricing service tests are implemented

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,59 @@
 package com.udacity.pricing;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import java.io.IOException;
 
-@RunWith(SpringRunner.class)
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @SpringBootTest
 public class PricingServiceApplicationTests {
+	private Long vehicleId = 5l;
 
 	@Test
-	public void contextLoads() {
+	public void testOKResponseStatusFromPricingService() throws IOException {
+		HttpUriRequest request = new HttpGet( "http://localhost:8082/prices/" + vehicleId);
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		assertTrue(response.getStatusLine().getStatusCode() == 200);
+	}
+
+	@Test
+	public void	testResponseMimeTypeFromPricingService() throws IOException {
+		HttpUriRequest request = new HttpGet( "http://localhost:8082/prices/" + vehicleId);
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+
+		assertEquals( "application/hal+json", mimeType );
+	}
+
+	@Test
+	public void getPriceFromPricingService() throws IOException, JSONException {
+		HttpUriRequest request = new HttpGet( "http://localhost:8082/prices/" + vehicleId);
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		JSONObject priceObject = new JSONObject(EntityUtils.toString(response.getEntity()));
+
+		assertTrue(Long.parseLong(priceObject.get("vehicleId").toString()) == vehicleId);
+	}
+
+	@Test
+	public void getAllPricesFromPricingService() throws IOException, JSONException {
+		HttpUriRequest request = new HttpGet( "http://localhost:8082/prices/");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+
+		JSONObject priceObject = new JSONObject(EntityUtils.toString(response.getEntity()));
+		assertTrue(priceObject.getJSONObject("_embedded").getJSONArray("prices").length() > 0);
 	}
 
 }
